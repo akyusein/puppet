e9589e0b99e4c9885afcaa45ce851677c590873d
Aksel Yusein <akselyusein@Aksels-MacBook-Pro.local>
2025-06-29 21:44:28 +0100

    SPOR-Puppet-9

---
+++ environments/production/modules/jenkins/manifests/config_file.pp
@@ -0,0 +1,16 @@
+define jenkins::config_file (
+  String $source_template,
+  String $path,
+  String $owner = 'jenkins',
+  String $group = 'jenkins',
+  String $mode  = '0644',
+) {
+  file { $path:
+    ensure  => file,
+    content => template($source_template),
+    owner   => $owner,
+    group   => $group,
+    mode    => $mode,
+    notify  => Service['jenkins'],
+  }
+}
