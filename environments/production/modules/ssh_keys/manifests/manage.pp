cfd950a1ed6bd7244808825869e25394d8801ca9
Aksel Yusein <akselyusein@Aksels-MacBook-Pro.local>
2025-07-17 02:46:00 +0300

    SPOR-Puppet-31

---
+++ environments/production/modules/ssh_keys/manifests/manage.pp
@@ -0,0 +1,32 @@
+class ssh_keys::manage {
+
+  $user = akselyusein
+
+  $user_key = lookup('vault::secret::ssh-keys::akselyusein::public_key')
+
+  user { $user:
+    ensure => present,
+    home   => "/home/${user}",
+    shell  => '/bin/bash',
+    managehome => true,
+    group => ['wheel'],
+    append => true,
+  }
+
+  file { "/home/${user}/.ssh":
+    ensure => 'directory',
+    owner  => $user,
+    group  => $user,
+    mode   => '0700',
+  }
+
+  file { "/home/${user}/.ssh/authorized_keys":
+    ensure  => 'present',
+    owner   => $user,
+    group   => $user,
+    mode    => '0600',
+    content => "${user_key}\n",
+  }
+}
+
+
